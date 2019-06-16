dbe92cc2ac64639f270bfc337b47e234d0a00acb
Mudassar Shafique <15931574+mudash@users.noreply.github.com>
2019-06-16 15:03:36 -0700

    Updates

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+WORKROOT=$(pwd)
+cd ${WORKROOT}
+
+# prepare PATH, GOROOT and GOPATH
+export PATH=$(pwd)/go/bin:$PATH
+export GOROOT=$(pwd)/go
+export GOPATH=$(pwd)
+
+go test -v $(GOPATH)/src/github.com/chef-partners/terraform-provisioner-habitat/habitat -v
+if [ $? -ne 0 ];
+then
+    echo "Failure in habitat provisioner unit tests"
+    exit 1
+fi
+echo "Successfully ran the unit tests for habitat provisioner"
+
